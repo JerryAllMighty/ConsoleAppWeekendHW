510e25e322aa9d6670acb4db474490f4686dbfd8
윤민 김 <dbsalszz@naver.com>
2020-09-25 18:38:04 +0900

    초기 커밋.

---
+++ ConsoleAppWeekendHW/MyCalc.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWeekendHW
+{
+    class MyCalc
+    {
+        public int result;
+        string answer;
+        string choice;
+
+        public int Plus(int a, int b)
+        {
+            result = a + b;
+            Console.WriteLine($"{a} + {b} = 뭣이당가?");
+            answer = Console.ReadLine().Trim().Replace(" ", "");
+
+            if (answer.Equals(""))
+            {
+                throw new Exception();
+            }
+            else if (answer == result.ToString())
+            {
+                Console.WriteLine("정답입니다! 계속 하시겠습니까? \t 1.예 \t 2.아니오");
+                choice = Console.ReadLine();
+
+                if (answer.Equals(""))
+                {
+                    throw new Exception();
+                }
+                else if (choice == "1")
+                {
+                    return result;
+                }
+                else if (choice == "2")
+                {
+                    Console.WriteLine("프로그램을 종료합니다");
+                    return 0;
+                }
+                else { throw new Exception(); }
+            }
+            
+            return result;
+        }
+
+        public int Minus(int a, int b)
+        {
+            result = a - b;
+            Console.WriteLine($"{a} - {b} = 뭣이당가?");
+            answer = Console.ReadLine().Trim().Replace(" ", "");
+
+            if (answer.Equals(""))
+            {
+                throw new Exception();
+            }
+            else if (answer == result.ToString())
+            {
+                Console.WriteLine("정답입니다! 계속 하시겠습니까? \t 1.예 \t 2.아니오");
+                choice = Console.ReadLine();
+
+                if (answer.Equals(""))
+                {
+                    throw new Exception();
+                }
+                else if (choice == "1")
+                {
+
+                }
+            }
+
+            return result;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            result = a * b;
+            Console.WriteLine($"{a} * {b} = 뭣이당가?");
+            answer = Console.ReadLine().Trim().Replace(" ", "");
+
+            if (answer.Equals(""))
+            {
+                throw new Exception();
+            }
+            else if (answer == result.ToString())
+            {
+                Console.WriteLine("정답입니다! 계속 하시겠습니까? \t 1.예 \t 2.아니오");
+                choice = Console.ReadLine();
+
+                if (answer.Equals(""))
+                {
+                    throw new Exception();
+                }
+                else if (choice == "1")
+                {
+
+                }
+            }
+
+            return result;
+        }
+
+        public int Divide(int a, int b)
+        {
+            result = a / b;
+            Console.WriteLine($"{a} / {b} = 뭣이당가");
+            answer = Console.ReadLine().Trim().Replace(" ", "");
+
+            if (answer.Equals(""))
+            {
+                throw new Exception();
+            }
+            else if (answer == result.ToString())
+            {
+                Console.WriteLine("정답입니다! 계속 하시겠습니까? \t 1.예 \t 2.아니오");
+                choice = Console.ReadLine();
+
+                if (answer.Equals(""))
+                {
+                    throw new Exception();
+                }
+                else if (choice == "1")
+                {
+
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleAppWeekendHW/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWeekendHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool bFlag = true;
+            MyCalc c1 = new MyCalc();
+            while (bFlag)
+            {
+                try
+                {
+                    Random rand = new Random();
+
+                    int firstNumber = rand.Next(10, 100);
+                    Console.WriteLine($"{firstNumber.ToString()}");
+                    int secondNumber = rand.Next(10, 100);
+                    Console.WriteLine($"{secondNumber.ToString()}");
+
+                      
+                    int function = rand.Next(1, 5);
+
+                    switch (function)
+                    {
+                        case 1:
+
+                            if (c1.Plus(firstNumber, secondNumber) == 0)
+                                bFlag = false;
+                            break;
+
+                        case 2:
+
+                            if (c1.Minus(firstNumber, secondNumber) == 0)
+                                bFlag = false;
+                            break;
+                        case 3:
+
+                            if(c1.Multiply(firstNumber, secondNumber)== 0)
+                                bFlag = false;
+                            break;
+                        case 4:
+
+                            if(c1.Divide(firstNumber, secondNumber) == 0)
+                                bFlag = false;
+                            break;
+
+                    }
+                    //
+
+                }
+                catch (Exception err)
+                { 
+                    Console.WriteLine(err.Message);
+                }
+
+
+            }
+        }
+    }
+}
