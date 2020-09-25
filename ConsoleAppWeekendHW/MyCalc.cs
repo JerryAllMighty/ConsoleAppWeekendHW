79b5543718b9a45cf5d49e794b8b971aa06907aa
윤민 김 <dbsalszz@naver.com>
2020-09-26 00:38:29 +0900

    숙제 최종본

--- ConsoleAppWeekendHW/MyCalc.cs
+++ ConsoleAppWeekendHW/MyCalc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,63 +12,186 @@ namespace ConsoleAppWeekendHW
         public int result;
         string answer;
         string choice;
+        bool bFlag = true;
+        bool cFlag = true;
+
 
         public int Plus(int a, int b)
         {
-            result = a + b;
-            Console.WriteLine($"{a} + {b} = 뭣이당가?");
-            answer = Console.ReadLine().Trim().Replace(" ", "");
-
-            if (answer.Equals(""))
+            while (cFlag)
             {
-                throw new Exception();
-            }
-            else if (answer == result.ToString())
-            {
-                Console.WriteLine("정답입니다! 계속 하시겠습니까? \t 1.예 \t 2.아니오");
-                choice = Console.ReadLine();
+                result = a + b;
+                Console.WriteLine($"{a} + {b} = 뭘까요?");
+                answer = Console.ReadLine().Trim().Replace(" ", "");
 
                 if (answer.Equals(""))
                 {
-                    throw new Exception();
+                    throw new NoNullAllowedException();
                 }
-                else if (choice == "1")
+                else if (answer == result.ToString())
                 {
-                    return result;
+                    Console.WriteLine("정답입니다!");
+                    while (bFlag)
+                    {
+                        Console.WriteLine("계속 하시겠습니까?");
+                        bFlag = true;
+
+                        Console.WriteLine("1.예 \t 2.아니오 \t 숫자로 입력해주세요");
+                        choice = Console.ReadLine().Trim().Replace(" ", "");
+
+                        if (!choice.Equals(""))
+                        {
+                            bFlag = false;
+                        }
+                        else
+                        { Console.WriteLine("다시 입력하세요"); }
+
+                    }
+
+                    if (choice == "1")
+                    {
+
+                        return result;
+                    }
+                    else if (choice == "2")
+                    {
+
+                        Console.WriteLine("프로그램을 종료합니다");
+
+                        return -1;
+                    }
+                    else
+                    {
+                        throw new Exception();
+
+                    }
+
                 }
-                else if (choice == "2")
+                else
                 {
-                    Console.WriteLine("프로그램을 종료합니다");
-                    return 0;
+                    Console.WriteLine("오답입니다. 사칙연산에 약하시네요.");
+                    //while (bFlag)
+                    //{
+
+
+                    //    //Console.WriteLine("계속 하시겠습니까?");
+                    //    //bFlag = true;
+
+                    //    //Console.WriteLine("1.예 \t 2.아니오 \t 숫자로 입력해주세요");
+                    //    //choice = Console.ReadLine().Trim().Replace(" ", "");
+
+                    //    //if (choice.Equals(""))
+                    //    //{
+                    //    //    Console.WriteLine("다시 입력하세요");
+                    //    //}
+                    //    //else if (choice == "1")
+                    //    //{
+                    //    //    return result;
+                    //    //}
+                    //    //else if (choice == "2")
+                    //    //{
+
+                    //    //    Console.WriteLine("프로그램을 종료합니다");
+
+                    //    //    return -1;
+                    //    //}
+                    //    //else
+                    //    //{
+                    //    //    throw new Exception();
+
+                    //    //}
+
+                    //}
                 }
-                else { throw new Exception(); }
             }
-            
+
             return result;
         }
 
         public int Minus(int a, int b)
         {
-            result = a - b;
-            Console.WriteLine($"{a} - {b} = 뭣이당가?");
-            answer = Console.ReadLine().Trim().Replace(" ", "");
-
-            if (answer.Equals(""))
+            while (cFlag)
             {
-                throw new Exception();
-            }
-            else if (answer == result.ToString())
-            {
-                Console.WriteLine("정답입니다! 계속 하시겠습니까? \t 1.예 \t 2.아니오");
-                choice = Console.ReadLine();
+                result = a - b;
+                Console.WriteLine($"{a} - {b} = 뭘까요?");
+                answer = Console.ReadLine().Trim().Replace(" ", "");
 
                 if (answer.Equals(""))
                 {
-                    throw new Exception();
+                    throw new NoNullAllowedException();
                 }
-                else if (choice == "1")
+                else if (answer == result.ToString())
                 {
+                    Console.WriteLine("정답입니다!");
+                    while (bFlag)
+                    {
+                        Console.WriteLine("계속 하시겠습니까?");
+                        bFlag = true;
 
+                        Console.WriteLine("1.예 \t 2.아니오 \t 숫자로 입력해주세요");
+                        choice = Console.ReadLine().Trim().Replace(" ", "");
+
+                        if (!choice.Equals(""))
+                        {
+                            bFlag = false;
+                        }
+                        else
+                        { Console.WriteLine("다시 입력하세요"); }
+
+                    }
+
+                    if (choice == "1")
+                    {
+
+                        return result;
+                    }
+                    else if (choice == "2")
+                    {
+
+                        Console.WriteLine("프로그램을 종료합니다");
+
+                        return 100000;
+                    }
+                    else
+                    {
+                        throw new Exception();
+
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("오답입니다. 사칙연산에 약하시네요.");
+                    //while (bFlag)
+                    //{
+                    //    Console.WriteLine("계속 하시겠습니까?");
+                    //    bFlag = true;
+
+                    //    Console.WriteLine("1.예 \t 2.아니오 \t 숫자로 입력해주세요");
+                    //    choice = Console.ReadLine().Trim().Replace(" ", "");
+
+                    //    if (choice.Equals(""))
+                    //    {
+                    //        Console.WriteLine("다시 입력하세요");
+                    //    }
+                    //    else if (choice == "1")
+                    //    {
+                    //        return result;
+                    //    }
+                    //    else if (choice == "2")
+                    //    {
+
+                    //        Console.WriteLine("프로그램을 종료합니다");
+
+                    //        return 100000;
+                    //    }
+                    //    else
+                    //    {
+                    //        throw new Exception();
+
+                    //    }
+
+                    //}
                 }
             }
 
@@ -76,26 +200,88 @@ namespace ConsoleAppWeekendHW
 
         public int Multiply(int a, int b)
         {
-            result = a * b;
-            Console.WriteLine($"{a} * {b} = 뭣이당가?");
-            answer = Console.ReadLine().Trim().Replace(" ", "");
-
-            if (answer.Equals(""))
+            while (cFlag)
             {
-                throw new Exception();
-            }
-            else if (answer == result.ToString())
-            {
-                Console.WriteLine("정답입니다! 계속 하시겠습니까? \t 1.예 \t 2.아니오");
-                choice = Console.ReadLine();
+                result = a * b;
+                Console.WriteLine($"{a} * {b} = 뭘까요?");
+                answer = Console.ReadLine().Trim().Replace(" ", "");
 
                 if (answer.Equals(""))
                 {
-                    throw new Exception();
+                    throw new NoNullAllowedException();
                 }
-                else if (choice == "1")
+                else if (answer == result.ToString())
                 {
+                    Console.WriteLine("정답입니다!");
+                    while (bFlag)
+                    {
+                        Console.WriteLine("계속 하시겠습니까?");
+                        bFlag = true;
 
+                        Console.WriteLine("1.예 \t 2.아니오 \t 숫자로 입력해주세요");
+                        choice = Console.ReadLine().Trim().Replace(" ", "");
+
+                        if (!choice.Equals(""))
+                        {
+                            bFlag = false;
+                        }
+                        else
+                        { Console.WriteLine("다시 입력하세요"); }
+
+                    }
+
+                    if (choice == "1")
+                    {
+
+                        return result;
+                    }
+                    else if (choice == "2")
+                    {
+
+                        Console.WriteLine("프로그램을 종료합니다");
+
+                        return -1;
+                    }
+                    else
+                    {
+                        throw new Exception();
+
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("오답입니다. 사칙연산에 약하시네요.");
+                    //while (bFlag)
+                    //{
+                    //    Console.WriteLine("계속 하시겠습니까?");
+                    //    bFlag = true;
+
+                    //    Console.WriteLine("1.예 \t 2.아니오 \t 숫자로 입력해주세요");
+                    //    choice = Console.ReadLine().Trim().Replace(" ", "");
+
+                    //    if (choice.Equals(""))
+                    //    {
+                    //        Console.WriteLine("다시 입력하세요");
+                    //    }
+                    //    else if (choice == "1")
+                    //    {
+                    //        return result;
+                    //    }
+                    //    else if (choice == "2")
+                    //    {
+
+                    //        Console.WriteLine("프로그램을 종료합니다");
+
+                    //        return -1;
+                    //    }
+                    //    else
+                    //    {
+                    //        throw new Exception();
+
+                    //    }
+
+                    //}
                 }
             }
 
@@ -104,26 +290,88 @@ namespace ConsoleAppWeekendHW
 
         public int Divide(int a, int b)
         {
-            result = a / b;
-            Console.WriteLine($"{a} / {b} = 뭣이당가");
-            answer = Console.ReadLine().Trim().Replace(" ", "");
-
-            if (answer.Equals(""))
+            while (cFlag)
             {
-                throw new Exception();
-            }
-            else if (answer == result.ToString())
-            {
-                Console.WriteLine("정답입니다! 계속 하시겠습니까? \t 1.예 \t 2.아니오");
-                choice = Console.ReadLine();
+                result = a / b;
+                Console.WriteLine($"{a} / {b} = 뭘까요?");
+                answer = Console.ReadLine().Trim().Replace(" ", "");
 
                 if (answer.Equals(""))
                 {
-                    throw new Exception();
+                    throw new NoNullAllowedException();
                 }
-                else if (choice == "1")
+                else if (answer == result.ToString())
                 {
+                    Console.WriteLine("정답입니다!");
+                    while (bFlag)
+                    {
+                        Console.WriteLine("계속 하시겠습니까?");
+                        bFlag = true;
 
+                        Console.WriteLine("1.예 \t 2.아니오 \t 숫자로 입력해주세요");
+                        choice = Console.ReadLine().Trim().Replace(" ", "");
+
+                        if (!choice.Equals(""))
+                        {
+                            bFlag = false;
+                        }
+                        else
+                        { Console.WriteLine("다시 입력하세요"); }
+
+                    }
+                    //비프래그는 현재 폴스
+                    if (choice == "1")
+                    {
+
+                        return result;
+                    }
+                    else if (choice == "2")
+                    {
+
+                        Console.WriteLine("프로그램을 종료합니다");
+
+                        return -1;
+                    }
+                    else
+                    {
+                        throw new Exception();
+
+                    }
+
+                }
+                else
+                {
+                    //Console.WriteLine("오답입니다. 사칙연산에 약하시네요.");
+                    //while (bFlag)
+                    //{
+                    //    Console.WriteLine("계속 하시겠습니까?");
+                    //    bFlag = true;
+
+                    //    Console.WriteLine("1.예 \t 2.아니오 \t 숫자로 입력해주세요");
+                    //    choice = Console.ReadLine().Trim().Replace(" ", "");
+
+                    //    if (choice.Equals(""))
+                    //    {
+                    //        Console.WriteLine("다시 입력하세요");
+                    //    }
+                    //    else if (choice == "1")
+                    //    {
+                    //        return result;
+                    //    }
+                    //    else if (choice == "2")
+                    //    {
+
+                    //        Console.WriteLine("프로그램을 종료합니다");
+
+                    //        return -1;
+                    //    }
+                    //    else
+                    //    {
+                    //        throw new Exception();
+
+                    //    }
+
+                    //}
                 }
             }
 
--- ConsoleAppWeekendHW/Program.cs
+++ ConsoleAppWeekendHW/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,51 +13,52 @@ namespace ConsoleAppWeekendHW
         {
             bool bFlag = true;
             MyCalc c1 = new MyCalc();
+            Random rand = new Random();
             while (bFlag)
             {
                 try
                 {
-                    Random rand = new Random();
-
                     int firstNumber = rand.Next(10, 100);
                     Console.WriteLine($"{firstNumber.ToString()}");
                     int secondNumber = rand.Next(10, 100);
                     Console.WriteLine($"{secondNumber.ToString()}");
 
-                        
-                    int function = rand.Next(1, 5);
+
+                    int function = rand.Next(1, 5);     //난수 연산자를 위해 생성
 
                     switch (function)
                     {
                         case 1:
-
-                            if (c1.Plus(firstNumber, secondNumber) == 0)
+                            if (c1.Plus(firstNumber, secondNumber) == -1)
                                 bFlag = false;
                             break;
 
                         case 2:
 
-                            if (c1.Minus(firstNumber, secondNumber) == 0)
+                            if (c1.Minus(firstNumber, secondNumber) == 100000)
                                 bFlag = false;
                             break;
                         case 3:
 
-                            if(c1.Multiply(firstNumber, secondNumber)== 0)
+                            if (c1.Multiply(firstNumber, secondNumber) == -1)
                                 bFlag = false;
                             break;
                         case 4:
 
-                            if(c1.Divide(firstNumber, secondNumber) == 0)
+                            if (c1.Divide(firstNumber, secondNumber) == -1)
                                 bFlag = false;
                             break;
 
                     }
-                    //
-
                 }
-                catch (Exception err)
-                { 
-                    Console.WriteLine(err.Message);
+
+                catch (NoNullAllowedException)
+                {
+                    Console.WriteLine("빈 값을 입력하셨습니다. 반드시 값을 입력해주세요.");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("똑바로 하시는 게 없으니 처음부터 다시 하겠습니다.");
                 }
 
 
